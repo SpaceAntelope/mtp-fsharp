9c4b73b7648f7ed42cea49b549e70dfac93d8bf5
SpaceAntilope <cernunos_kav@msn.com>
2015-05-07 14:35:40 +0300

    interop still no worky

--- PortableF/Program.fs
+++ PortableF/Program.fs
@@ -1,7 +1,23 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
+open PortableDeviceApiLib
+open PortableDeviceTypesLib
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+        
+    let deviceManager = PortableDeviceManagerClass()
+    deviceManager.RefreshDeviceList()
+    let deviceIDs = [|""|] 
+    let deviceIDsCountRef = ref (uint32 0)
+
+    deviceManager.GetDevices(deviceIDs , deviceIDsCountRef)
+    printfn "result = %d %A" !deviceIDsCountRef deviceIDs
+    //deviceManager.GetDevices(deviceIDs , &deviceIDsCountRef)
+    //printfn "result = %d %A" deviceIDsCountRef deviceIDs
+
+    System.Console.ReadLine()
+
     0 // return an integer exit code
