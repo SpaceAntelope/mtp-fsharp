cbb92e0badffb0930b429374de3c9204465a1f68
Ares Terzopoulos <cernunos_kav@msn.com>
2015-08-07 03:12:03 +0300

    Renamed sample folder

--- FsPortableDevices.Sample.IO/AssemblyInfo.fs
+++ FsPortableDevices.Sample.IO/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace Sample.IO.AssemblyInfo
+﻿namespace FsPortableDevices.Sample.IO.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("Sample.IO")>]
+[<assembly: AssemblyTitle("FsPortableDevices.Sample.IO")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("Sample.IO")>]
+[<assembly: AssemblyProduct("FsPortableDevices.Sample.IO")>]
 [<assembly: AssemblyCopyright("Copyright ©  2015")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
